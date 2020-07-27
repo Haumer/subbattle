bf02fa1b0e73d43db61e54c96948808b7c32b473
alexander <alexander.haumer@me.com>
2020-07-27 23:27:36 +0100

    score counter

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :battles, only: [ :show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post "declare_home_winner", to: "matches#declare_home_winner"
-  post "declare_away_winner", to: "matches#declare_away_winner"
   post "declare_winner", to: "matches#declare_winner", as: "declare_winner"
   get "battles/:id/start", to: "battles#start", as: "start"
 end
