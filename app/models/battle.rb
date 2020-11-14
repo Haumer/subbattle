c01a244fabbfbfe57875895338675138e38b5955
alexander <alexander.haumer@me.com>
2020-11-14 17:09:41 +0000

    adding scoring logic

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,4 +2,15 @@ class Battle < ApplicationRecord
   belongs_to :home_team, class_name: "Team"
   belongs_to :away_team, class_name: "Team"
   has_many :matches
+
+  def score_up_to_now(nr_of_matches_played)
+    return unless nr_of_matches_played
+
+    difference = Hash.new(0)
+    matches.all.first(nr_of_matches_played).each do |match|
+      difference[:home] += match.result.first
+      difference[:away] += match.result.last
+    end
+    difference
+  end
 end
